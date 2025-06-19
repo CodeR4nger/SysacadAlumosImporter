56f820c3c0661903abec9e17d4b5ca6b42cc18e6
CodeR4nger <ignabianchi@gmail.com>
2025-06-19 17:06:38 -0300

    Programa principal

--- AlumnosCSVImporter/Program.cs
+++ AlumnosCSVImporter/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using AlumnosCSVImporter.Data;
+using AlumnosCSVImporter.Import;
+using AlumnosCSVImporter.Repositories;
+using AlumnosCSVImporter.Services;
+
+Console.WriteLine("IMPORTADOR CSV");
+const string FILE_PATH = "alumnos.csv";
+
+Console.WriteLine("Conectando a base de datos");
+var databaseContext = new DatabaseContextFactory().CreateContext();
+Console.WriteLine("Conexion establecida exitosamente");
+Console.WriteLine("Verificando tabla Alumnos");
+databaseContext.EnsureAlumnoTable();
+Console.WriteLine("Creando importador");
+var stopwatch = Stopwatch.StartNew();
+var importer = new AlumnoImporter(new AlumnoService(new AlumnoRepository(databaseContext)));
+Console.WriteLine("Iniciando importacion");
+importer.Import(FILE_PATH);
+Console.WriteLine("Importacion finalizada exitosamente");
+stopwatch.Stop();
+Console.WriteLine($"Tiempo transcurrido: {stopwatch.ElapsedMilliseconds} ms");
